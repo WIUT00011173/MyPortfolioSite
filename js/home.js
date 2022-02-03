911e16061238ef7e866ba86e10f30f781edadedd
WIUT00011173 <wiut00011173student@gmail.com>
2022-02-03 13:36:45 +0500

    about section completed

--- js/home.js
+++ js/home.js
@@ -10,3 +10,16 @@ const loader = document.querySelector("#preloader")
 window.addEventListener("load", ()=> {
     loader.style.display = "none"
 })
+
+
+// ABOUT SECTION JS:
+
+const wiutImg = document.querySelector(".wiutimg")
+const overlay = document.querySelector(".overlay")
+
+wiutImg.addEventListener("mouseover", function() {
+    overlay.classList.add("overlayShow")
+})
+wiutImg.addEventListener("mouseout", function() {
+    overlay.classList.remove("overlayShow")
+})
