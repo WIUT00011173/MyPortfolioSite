641a402b6f3f0cbf6d824372802943d3a0520772
WIUT00011173 <wiut00011173student@gmail.com>
2022-02-15 17:21:35 +0500

    close navbar funtion

--- js/home.js
+++ js/home.js
@@ -1,14 +1,30 @@
 const navbar  = document.querySelector(".navbar")
+const ul  = document.querySelector(".ul")
 const menuIcon = document.querySelector(".close-open")
 
-menuIcon.addEventListener("click", () => {
+menuIcon.addEventListener("click", () => close())
+
+ul.addEventListener("click", () => close())
+
+function close() {
     navbar.classList.toggle("showNavbar")
     menuIcon.classList.toggle("close-menu")
-})
+}
+
+
 
 const loader = document.querySelector("#preloader")
 window.addEventListener("load", ()=> {
     loader.style.display = "none"
+})
+
+
+// Scroll effect header:
+
+window.addEventListener('scroll', function () {
+  let header = document.querySelector('header');
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle('scrolling-active', windowPosition);
 })
 
 
